9d9727154b33750012e7ca38bb2c26f14c9c07f6
olutola olajide michael <olutolajide@gmail.com>
2023-03-11 16:12:02 +0000

    reconfigure recovery,signup and confirm page

--- frontend-react-js/src/pages/RecoverPage.js
+++ frontend-react-js/src/pages/RecoverPage.js
@@ -2,6 +2,7 @@ import './RecoverPage.css';
 import React from "react";
 import {ReactComponent as Logo} from '../components/svg/logo.svg';
 import { Link } from "react-router-dom";
+import { Auth } from 'aws-amplify';
 
 export default function RecoverPage() {
   // Username is Eamil
@@ -11,17 +12,6 @@ export default function RecoverPage() {
   const [code, setCode] = React.useState('');
   const [errors, setErrors] = React.useState('');
   const [formState, setFormState] = React.useState('send_code');
-
-  const onsubmit_send_code = async (event) => {
-    event.preventDefault();
-    console.log('onsubmit_send_code')
-    return false
-  }
-  const onsubmit_confirm_code = async (event) => {
-    event.preventDefault();
-    console.log('onsubmit_confirm_code')
-    return false
-  }
 
   const username_onchange = (event) => {
     setUsername(event.target.value);
@@ -34,6 +24,28 @@ export default function RecoverPage() {
   }
   const code_onchange = (event) => {
     setCode(event.target.value);
+  }
+
+  const onsubmit_send_code = async (event) => {
+    event.preventDefault();
+    setErrors('')
+    Auth.forgotPassword(username)
+    .then((data) => setFormState('confirm_code') )
+    .catch((err) => setErrors(err.message) );
+    return false
+  }
+
+  const onsubmit_confirm_code = async (event) => {
+    event.preventDefault();
+    setErrors('')
+    if (password == passwordAgain){
+      Auth.forgotPasswordSubmit(username, code, password)
+      .then((data) => setFormState('success'))
+      .catch((err) => setErrors(err.message) );
+    } else {
+      setErrors('Passwords do not match')
+    }
+    return false
   }
 
   let el_errors;
