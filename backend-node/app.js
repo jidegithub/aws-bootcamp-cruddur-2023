0e309ddedddcabac986a9312af5ec262b705ad97
olajide.o <olutolajide@gmail.com>
2023-03-14 14:38:47 +0100

    add express app

---
+++ backend-node/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(
+    `<h1>Docker is fun!</h1>`
+  )
+})
+
+app.use((err, req, res, next) => {
+  if (err) {
+    console.error(err);
+    res
+      .status(err.statusCode || err.status || 500)
+      .send(err || {});
+  } else {
+    next();
+  }
+});
+
+const server = app.listen(8000, () => {
+  console.log('App listening on port', server.address().port);
+});
