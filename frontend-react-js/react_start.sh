15e2a152722eaf8092f043b2793b2313d4505ddb
olutola olajide michael <42746141+jidegithub@users.noreply.github.com>
2023-03-15 15:54:03 +0000

    update script

--- frontend-react-js/react_start.sh
+++ frontend-react-js/react_start.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+unset REACT_APP_BACKEND_URL
 change_cde (){ 
-    if [ "$USER" == "codespace" ]; then 
-        $REACT_APP_BACKEND_URL = "https://$CODESPACE_NAME-4567.$GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN" 
+    if [ "$USER" == "codespace" ]; then
+        echo "werepe!!" 
+        export REACT_APP_BACKEND_URL=https://$CODESPACE_NAME-4567.$GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN
     fi
 }
 
-# change_cde
+change_cde
