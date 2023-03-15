70c8c1be7998c3c5a2bfda6d42fe37ed7dedcaba
olajide.o <olutolajide@gmail.com>
2023-03-15 21:29:09 +0100

    set up env varaible for react

---
+++ frontend-react-js/start-app.js
@@ -0,0 +1,12 @@
+// load environment variables from .env file
+require('dotenv').config();
+
+// check the value of USER and modify it if necessary
+if (process.env.USER === 'codespace') {
+  process.env.REACT_APP_BACKEND_URL = `https://${CODESPACE_NAME}-4567.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`;
+} else if (process.env.USER === 'gitpod') {
+  process.env.REACT_APP_BACKEND_URL = `https://4567-${GITPOD_WORKSPACE_ID}.${GITPOD_WORKSPACE_CLUSTER_HOST}`;
+}
+
+// start the React app
+require('react-scripts/scripts/start');
