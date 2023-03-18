f94304eb07a22d542aec7fa7af23cca08bacc84a
olajide.o <olutolajide@gmail.com>
2023-03-18 20:21:42 +0100

    post activities

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
-from lib.db import pool, query_wrap_array
+from lib.db import db
 
 tracer = trace.get_tracer("home.activities")
 class HomeActivities:
@@ -12,30 +12,7 @@ class HomeActivities:
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
 
-      sql = query_wrap_array("""
-      SELECT
-        activities.uuid,
-        users.display_name,
-        users.handle,
-        activities.message,
-        activities.replies_count,
-        activities.reposts_count,
-        activities.likes_count,
-        activities.reply_to_activity_uuid,
-        activities.expires_at,
-        activities.created_at
-      FROM public.activities
-      LEFT JOIN public.users ON users.uuid = activities.user_uuid
-      ORDER BY activities.created_at DESC
-      """)
-
-      with pool.connection() as conn:
-        with conn.cursor() as cur:
-          cur.execute(sql)
-          # this will return a tuple
-          # the first field being the data
-          json = cur.fetchone()
-          #HoneyCom Span tracer("HomeActivities")
-          span.set_attribute("app.result_length", len(json[0]))
-      return json[0]
+      sql = db.template('activities','home')
+      results = db.query_array_json(sql)
+      return results
       
