64b0d64375ddeb3844a99555e0a2409abd29d299
olutola olajide michael <42746141+jidegithub@users.noreply.github.com>
2023-03-19 17:43:43 +0000

    fixing otel errors

--- frontend-react-js/src/tracing.js
+++ frontend-react-js/src/tracing.js
@@ -4,12 +4,19 @@ import { WebTracerProvider, BatchSpanProcessor } from '@opentelemetry/sdk-trace-
 import { ZoneContextManager } from '@opentelemetry/context-zone';
 import { Resource }  from '@opentelemetry/resources';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+
+// For sending traces for all http requests
+import { XMLHttpRequestInstrumentation } from '@opentelemetry/instrumentation-xml-http-request';
+import { FetchInstrumentation } from '@opentelemetry/instrumentation-fetch';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load';
 
 const exporter = new OTLPTraceExporter({
-  url: process.env.REACT_APP_FRONTEND_URL,
-  headers: {
-    "x-honeycomb-team": process.env.HONEYCOMB_API_KEY_FRONTEND,
-  },
+  url: `${process.env.REACT_APP_FRONTEND_URL}:443/v1/traces`
+  // headers: {
+  //   "x-honeycomb-team": process.env.HONEYCOMB_API_KEY_FRONTEND,
+  // },
 });
 
 console.log(`Connecting to ${process.env.REACT_APP_FRONTEND_URL}/v1/traces collector`)
@@ -24,4 +31,21 @@ provider.addSpanProcessor(new BatchSpanProcessor(exporter));
 
 provider.register({
   contextManager: new ZoneContextManager()
+});
+
+// Registering instrumentations / plugins
+registerInstrumentations({
+  instrumentations: [
+    new XMLHttpRequestInstrumentation({
+      propagateTraceHeaderCorsUrls: [
+        new RegExp(`${process.env.REACT_APP_BACKEND_URL}`, 'g')
+      ]
+    }),
+    new FetchInstrumentation({
+      propagateTraceHeaderCorsUrls: [
+        new RegExp(`${process.env.REACT_APP_BACKEND_URL}`, 'g')
+      ]
+    }),
+    new DocumentLoadInstrumentation(),
+  ],
 });
