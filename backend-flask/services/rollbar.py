2ba65979311a4d1bb0d8f6253349789ab252dc10
olutola olajide michael <olutolajide@gmail.com>
2023-03-22 22:57:44 +0000

    fixes

--- backend-flask/services/rollbar.py
+++ backend-flask/services/rollbar.py
@@ -4,17 +4,13 @@ from flask import Flask
 from flask import got_request_exception
 import os
 
-app = Flask.app_context()
-
-with app:
-  @app.before_first_request
-  def init_rollbar(app: app):
-    rollbar.init(
-    access_token=os.getenv("ROLLBAR_ACCESS_TOKEN"),
-    environment='cruddur flask',
-    root=os.path.dirname(os.path.realpath(__file__)),
-    code_version='1.0',
-    allow_logging_basic_config=False
-    )
-    # send exceptions from `app` to rollbar, using flask's signal system.
-    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+def init_rollbar(app: Flask):
+  rollbar.init(
+  access_token=os.getenv("ROLLBAR_ACCESS_TOKEN"),
+  environment='cruddur flask',
+  root=os.path.dirname(os.path.realpath(__file__)),
+  code_version='1.0',
+  allow_logging_basic_config=False
+  )
+  # send exceptions from `app` to rollbar, using flask's signal system.
+  got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
