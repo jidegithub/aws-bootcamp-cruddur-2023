1ae30d9221eb5e1105108a0da1886be9c5e5ad83
olajide.o <olutolajide@gmail.com>
2023-03-23 23:18:35 +0100

    fix

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,7 +3,6 @@ INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('Grahams Tartar', 'grahams', 'grahams-tartar.0x@icloud.com', 'MOCK'),
   ('Wriggle Reef', 'reef', 'wriggle.reef_0m@icloud.com', 'MOCK'),
-  -- ('Chitty Bang', 'chitty', 'chittybang@exampro.co', 'MOCK'),
   ('Londo Mollari', 'lmollari@centari.com','londo','MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
