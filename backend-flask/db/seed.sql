90055ceb1cc8c3f8c8b79cb8dbb2a1c3abf7bc38
olutola olajide michael <olutolajide@gmail.com>
2023-03-23 15:17:31 +0000

    update files

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,6 +3,8 @@ INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrewbrown', 'andrewbrown@exampro.co', 'MOCK'),
   ('Andrew Bayko', 'andrewbayko', 'andrewbayko@exampro.co', 'MOCK'),
+  ('Grahams Tartar', 'grahams', 'grahams-tartar.0x@icloud.com', 'MOCK'),
+  ('Wriggle Reef', 'reef', 'wriggle.reef_0m@icloud.com', 'MOCK'),
   ('Chitty Bang', 'chitty', 'chittybang@exampro.co', 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
