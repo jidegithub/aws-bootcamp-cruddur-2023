48b353598d18f2885f8a4aa2deded35d8a8d9037
olutola olajide michael <olutolajide@gmail.com>
2023-03-23 19:02:45 +0000

    modified seed sql with my cognito users

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,6 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown', 'andrewbrown@exampro.co', 'MOCK'),
-  ('Andrew Bayko', 'andrewbayko', 'andrewbayko@exampro.co', 'MOCK'),
   ('Grahams Tartar', 'grahams', 'grahams-tartar.0x@icloud.com', 'MOCK'),
   ('Wriggle Reef', 'reef', 'wriggle.reef_0m@icloud.com', 'MOCK'),
   ('Chitty Bang', 'chitty', 'chittybang@exampro.co', 'MOCK');
