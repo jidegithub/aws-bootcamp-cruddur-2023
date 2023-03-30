89520bd71f80fa3141f58743fc237a1b899bb88c
olutola olajide michael <olutolajide@gmail.com>
2023-03-30 15:34:49 +0000

    fix

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -227,7 +227,7 @@ def health():
 @app.after_request
 def after_request(response):
   timestamp = strftime('[%Y-%b-%d %H:%M]')
-  logger.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+  logger.info('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
   return response
 
 if __name__ == "__main__":
