b4d939835dbc9712068afebf8a85edbe35af7b29
olutola olajide michael <42746141+jidegithub@users.noreply.github.com>
2023-04-10 23:38:21 +0000

    modify sg rule route in post create script

--- .devcontainer/onPostCreateCommand.sh
+++ .devcontainer/onPostCreateCommand.sh
@@ -2,4 +2,4 @@
 
 # AWS set current space ip address and update rds security group rule
 export ENVIRONMENT_IP=$(curl ifconfig.me)
-source  "/workspaces/aws-bootcamp-cruddur-2023/backend-flask/bin/rds/update-sg-rule"
+source  "/workspaces/aws-bootcamp-cruddur-2023/bin/rds/update-sg-rule"
