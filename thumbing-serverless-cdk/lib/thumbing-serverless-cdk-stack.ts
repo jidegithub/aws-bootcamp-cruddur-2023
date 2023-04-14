ed9a73c9c16bf2daa19eb8649e278fa445343664
olutola olajide michael <olutolajide@gmail.com>
2023-04-14 21:11:23 +0000

    setup bucket

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -9,12 +9,14 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
 
     // The code that defines your stack goes here
     const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
+    const bucket = this.createBucket(bucketName)
   }
 
-  createBucket(bucketName: string){
+  createBucket(bucketName: string): s3.IBucket {
     const bucket = new s3.Bucket(this, 'ThumbingBucket', {
       bucketName: bucketName,
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
+    return bucket;
   }
 }
