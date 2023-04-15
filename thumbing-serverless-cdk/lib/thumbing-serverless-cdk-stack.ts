296ccaba2b36940957dd4eb6b3099608fe3dc6f1
olutola olajide michael <olutolajide@gmail.com>
2023-04-15 10:39:58 +0000

    process image lambda

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -2,27 +2,37 @@ import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as lambda from 'aws-cdk-lib/aws-lambda'
 import { Construct } from 'constructs';
-import * as dotenv from 'dotenv';
+// import * as dotenv from 'dotenv';
+const dotenv = require('dotenv');
 
 //load env variables
-dotenv.config()
+dotenv.config({path:'./example.env'})
 
 export class ThumbingServerlessCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
     const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
-    const functionPath: string = process.env.THUMBING_FUNCTION_PATH as string;
     const folderInput: string = process.env.THUMBING_S3_FOLDER_INPUT as string;
     const folderOutput: string = process.env.THUMBING_S3_FOLDER_OUTPUT as string;
-
+    const webhookUrl: string = process.env.THUMBING_WEBHOOK_URL as string;
+    const topicName: string = process.env.THUMBING_TOPIC_NAME as string;
+    const functionPath: string = process.env.THUMBING_FUNCTION_PATH as string;
+    console.log('bucketName',bucketName)
+    console.log('folderInput',folderInput)
+    console.log('folderOutput',folderOutput)
+    console.log('webhookUrl',webhookUrl)
+    console.log('topicName',topicName)
+    console.log('functionPath',functionPath)
+    
+    // The code that defines your stack goes here
     const bucket = this.createBucket(bucketName)
     const lambda = this.createLambda(functionPath, bucketName, folderInput, folderOutput)
   }
 
   createBucket(bucketName: string): s3.IBucket {
-    const bucket = new s3.Bucket(this, 'ThumbingBucket', {
+    const logicalName: string = 'ThumbingBucket';
+    const bucket = new s3.Bucket(this, logicalName, {
       bucketName: bucketName,
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
