4069c3f066c4ed6d27da6df4135e2fc106fcb890
olajide.o <olutolajide@gmail.com>
2023-04-17 15:33:32 +0100

    codespaces config

--- .devcontainer/onPostCreateCommand.sh
+++ .devcontainer/onPostCreateCommand.sh
@@ -1,25 +1,12 @@
 #!/usr/bin/env bash
 
-# AWS set current space ip address and update rds security group rule
-export ENVIRONMENT_IP=$(curl ifconfig.me)
-source "/workspaces/aws-bootcamp-cruddur-2023/bin/rds/update-sg-rule"
-
 #generate env for Frontend Reactjs
-source /workspaces/aws-bootcamp-cruddur-2023/bin/frontend/generate-env
+source /workspaces/aws-bootcamp-cruddur-2023/bin/frontend/generate_env
 
 #generate env for Backend Flask
-source /workspaces/aws-bootcamp-cruddur-2023/bin/backend/generate-env
+source /workspaces/aws-bootcamp-cruddur-2023/bin/backend/generate_env
 
 #fargate task utils
 curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
 sudo dpkg -i session-manager-plugin.deb
 
-#aws CDK utils
-npm install aws-cdk -g
-cd /workspaces/aws-bootcamp-cruddur-2023/thumbing-serverless-cdk;
-cp .env.example .env
-npm i
-
-#process-images
-cd /workspaces/aws-bootcamp-cruddur-2023/aws/lambdas/process-images;
-source /workspaces/aws-bootcamp-cruddur-2023/bin/avatar/sharp
