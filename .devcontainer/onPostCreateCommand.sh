8e53b6d494297bfa3d87a225f23b04ccefe186ef
olajide.o <olutolajide@gmail.com>
2023-04-17 14:34:15 +0100

    generate env dyamically

--- .devcontainer/onPostCreateCommand.sh
+++ .devcontainer/onPostCreateCommand.sh
@@ -3,3 +3,17 @@
 # AWS set current space ip address and update rds security group rule
 export ENVIRONMENT_IP=$(curl ifconfig.me)
 source "/workspaces/aws-bootcamp-cruddur-2023/bin/rds/update-sg-rule"
+
+#fargate task utils
+curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
+sudo dpkg -i session-manager-plugin.deb
+
+#aws CDK utils
+npm install aws-cdk -g
+cd /workspaces/aws-bootcamp-cruddur-2023/thumbing-serverless-cdk;
+cp .env.example .env
+npm i
+
+#process-images
+cd /workspaces/aws-bootcamp-cruddur-2023/aws/lambdas/process-images;
+source "/workspaces/aws-bootcamp-cruddur-2023/bin/avatar/sharp"
