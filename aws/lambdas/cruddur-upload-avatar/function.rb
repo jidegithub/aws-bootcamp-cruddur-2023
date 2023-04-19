1e9f0c5de112ecd241b523255e017368ab9fc699
olutola olajide michael <42746141+jidegithub@users.noreply.github.com>
2023-04-19 18:58:29 +0000

    cruddur avatar upload fix

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -9,6 +9,14 @@ def handler(event:, context:)
 
 	obj = s3.bucket(bucket_name).object(object_key)
 	url = obj.presigned_url(:put, expires_in: 60 * 5)
-	body = {url: url} #will be returned
-	{ statusCode: 200, body: body }
+	body = {url: url}.to_json #will be returned
+	{ 
+      headers: {
+        "Access-Control-Allow-Headers": "*, Authorization",
+        "Access-Control-Allow-Origin": "https://jidegithub-fantastic-giggle-g7qrxgj55rgh9r77-4567.preview.app.github.dev",
+        "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
+      },
+      statusCode: 200,
+	  body: body
+    }
 end
