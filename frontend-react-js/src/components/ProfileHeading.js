fbe8a5070909ad9913b9ed127e6c9cfd70eb4342
olajide.o <olutolajide@gmail.com>
2023-04-20 06:27:44 +0100

    upload user avatar

--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -1,10 +1,9 @@
 import './ProfileHeading.css';
-import EditProfileButton from '../components/EditProfileButton';
-
-// import ProfileAvatar from 'components/ProfileAvatar'
+import EditProfileButton from 'components/EditProfileButton';
+import ProfileAvatar from 'components/ProfileAvatar'
 
 export default function ProfileHeading(props) {
-  const backgroundImage = 'url("https://assets.cruddur.com/banners/banner.jpg")';
+  const backgroundImage = 'url("https://assets.jidecruddur.site/banners/piratesofthecarribean.jpeg")';
   const styles = {
     backgroundImage: backgroundImage,
     backgroundSize: 'cover',
@@ -15,7 +14,7 @@ export default function ProfileHeading(props) {
     <div className='title'>{props.profile.display_name}</div>
     <div className="cruds_count">{props.profile.cruds_count} Cruds</div>
     <div className="banner" style={styles} >
-      {/* <ProfileAvatar id={props.profile.cognito_user_uuid} /> */}
+      <ProfileAvatar id={props.profile.cognito_user_uuid} />
     </div>
     <div className="info">
       <div className='id'>
