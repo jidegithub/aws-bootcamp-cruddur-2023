be0da069724b3732ec120ec2e773a9cefed5b1b1
olutola olajide michael <olutolajide@gmail.com>
2023-06-20 07:24:05 +0000

    fix banner image pathing

--- frontend-react-js/src/components/ProfileAvatar.js
+++ frontend-react-js/src/components/ProfileAvatar.js
@@ -1,7 +1,7 @@
 import './ProfileAvatar.css';
 
 export default function ProfileAvatar(props) {
-  const backgroundImage = `url("https://d14trefe5nds05.cloudfront.net/avatars/processed/${props.id}.jpg")`;
+  const backgroundImage = `url("https://d14trefe5nds05.cloudfront.net/avatars/${props.id}.jpg")`;
   const styles = {
     backgroundImage: backgroundImage,
     backgroundSize: 'cover',
