09c7fac44d9b5b50281674f848a7c276b43bc6d8
olutola olajide michael <olutolajide@gmail.com>
2023-06-20 05:52:09 +0000

    fix logger error

--- backend-flask/routes/general.py
+++ backend-flask/routes/general.py
@@ -3,6 +3,6 @@ from flask import request
 def load(app): 
 	@app.route("/api/health-check", methods=["GET"])
 	def health_check():
-		logger.info("health request received")
+		app.logger.info("health request received")
 		data = {"success": True, "message": "healthy"}
 		return data, 200
