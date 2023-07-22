fbce8abd75efad347389849cb5329fd74478ef19
olutola olajide michael <olutolajide@gmail.com>
2023-07-22 22:48:04 +0000

    rollbar config

--- backend-flask/services/logging/rollbar.py
+++ backend-flask/services/logging/rollbar.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask import got_request_exception
 import os
 
-## XXX hack to make request data work with pyrollbar <= 0.16.3
+## hack to make request data work with pyrollbar <= 0.16.3
 def _get_flask_request():
   print("Getting flask request")
   from flask import request
@@ -15,16 +15,18 @@ rollbar._get_flask_request = _get_flask_request
 def _build_request_data(request):
   return rollbar._build_werkzeug_request_data(request)
 rollbar._build_request_data = _build_request_data
-## XXX end hack
+## end hack
 
 def init_rollbar(app: Flask):
-  rollbar.init(
   access_token=os.getenv("ROLLBAR_ACCESS_TOKEN"),
-  flask_env = os.getenv('FLASK_ENV'),
-  environment='cruddur flask',
-  root=os.path.dirname(os.path.realpath(__file__)),
-  code_version='1.0',
-  allow_logging_basic_config=False
+  flask_env = os.getenv('FLASK_ENV') or 'production',
+  rollbar.init(
+    # access token
+    access_token,
+    # environment name
+    flask_env,
+    root=os.path.dirname(os.path.realpath(__file__)),
+    allow_logging_basic_config=False
   )
   # send exceptions from `app` to rollbar, using flask's signal system.
   got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
